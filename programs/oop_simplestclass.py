dfcd90444d53b2f216899eba744fd63c631f9768
5byg5oyv6Ziz <147676052+5byg5oyv6Ziz@users.noreply.github.com>
2023-10-17 14:48:15 +0800

    Update oop_simplestclass.py

--- programs/oop_simplestclass.py
+++ programs/oop_simplestclass.py
@@ -1,5 +1,33 @@
-class Person:
-    pass  # An empty block
 
-p = Person()
-print(p)
+import flag
+
+
+for i in range(len(flag)):
+    print(ord(flag[i])^ord(flag[(i+1)%len(flag)]),end="\n")
+'''54
+0
+48
+54
+23
+18
+29
+24
+35
+46
+49
+38
+73
+69
+42
+42
+6
+22
+58
+47
+9
+38
+39
+72
+66
+15
+46'''
